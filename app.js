aafd3670c2f1459ccbd2ff70631a27abbda25b8e
Aviral Singh Pal <65078542+AviralSinghPal@users.noreply.github.com>
2023-01-10 15:13:41 +0000

    adding middle wares

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
 const express = require('express')
+const fileUpload = require('express-fileupload')
 const morgan = require('morgan')
 const home  = require('./routes/home')
 
 const app = express()
 
-app.use(morgan("tiny"))
+app.use(cookieParser())
+app.use(fileUpload())
+app.use(morgan("tiny"))//morgan middle ware
+
+//regular middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
 
 app.get("/",(req,res)=>{
     res.send(`Ispe nahi API/v1 PE JAO`)
