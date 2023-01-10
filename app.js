5167edd0902f817156c44b1fccbdd8d2ced12ea2
Aviral Singh Pal <65078542+AviralSinghPal@users.noreply.github.com>
2023-01-10 15:02:21 +0000

    added morgan

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const morgan = require('morgan')
 const home  = require('./routes/home')
 
 const app = express()
 
-
+app.use(morgan("tiny"))
 
 app.get("/",(req,res)=>{
     res.send(`Ispe nahi API/v1 PE JAO`)
