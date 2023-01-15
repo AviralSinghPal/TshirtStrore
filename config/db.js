81dd06d85e967eabd80bcb9f070e1cb5e218060a
Aviral Singh Pal <65078542+AviralSinghPal@users.noreply.github.com>
2023-01-15 07:32:16 +0000

    DB connection Fix

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
 const connectWithDb = ()=>{
+    mongoose.set('strictQuery', false);
     mongoose.connect(process.env.DB_URL,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(console.log(`DB got Connected`))
     .catch((error)=>{
-        console.log(`DV connection issues`);
+        console.log(`DB connection issues`);
         console.log(error);
         process.exit(1);
     })
